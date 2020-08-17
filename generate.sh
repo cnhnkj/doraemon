db26dfac939b5b184ad00f1028043dcf3c728a32
dragonlong1986 <dragonlong1986@126.com>
2020-08-17 22:26:13 +0800

    增加上传文件的consume协议

--- generate.sh
+++ generate.sh
@@ -6,5 +6,6 @@ mvn clean package
 java -jar doraemon-generator/target/doraemon-generator.jar $1
 
 mvn clean deploy --projects doraemon-api -amd
+# mvn clean install --projects doraemon-api -amd
 
 
