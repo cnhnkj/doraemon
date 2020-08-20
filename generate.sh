6480564f540c384f61ec5db4198ebe1368e547dd
dragonlong1986 <dragonlong1986@126.com>
2020-08-20 17:03:59 +0800

    添加直播的接口

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 例子: bash generate.sh coins,paradise,hook-service,user-vip,supply,order-query,order-create
+# 例子: bash generate.sh coins,paradise,hook-service,user-vip,supply,order-query,order-create,hn-live
 
 mvn clean package
 java -jar doraemon-generator/target/doraemon-generator.jar $1
