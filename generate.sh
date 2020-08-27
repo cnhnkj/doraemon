e6695c10b12c8b557e8e606ead091a1c6fe3e2c2
dragonlong1986 <dragonlong1986@126.com>
2020-08-27 17:57:40 +0800

    加入imcore-poly

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 例子: bash generate.sh coins,paradise,hook-service,user-vip,supply,order-query,order-create,hn-live,phoenix,shop-mono,market-platform,message-center
+# 例子: bash generate.sh coins,paradise,hook-service,user-vip,supply,order-query,order-create,hn-live,phoenix,shop-mono,market-platform,message-center,imcore-poly
 
 mvn clean package
 java -jar doraemon-generator/target/doraemon-generator.jar $1
