600a864a86d352af5ac3fee4f29f71303f7a8d63
dragonlong1986 <dragonlong1986@126.com>
2020-09-02 11:32:55 +0800

    升级到1.3.0版本

--- generate.sh
+++ generate.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-# 例子: bash generate.sh coins,paradise,hook-service,user-vip,supply,order-query,order-create,hn-live,phoenix,shop-mono,market-platform,message-center,imcore-poly
+# 1.0.0
+# bash generate.sh coins,userVip,hookService,paradise
 
-mvn clean package
+# 1.1.0
+# bash generate.sh supply,order-query,order-create,hn-live
+
+# 1.2.0
+# bash generate.sh phoenix,shop-mono,market-platform,message-center,imcore-poly
+
+# 1.3.0
+# bash generate.sh communication,wechat-foreground,imdatacenter-mono
+
+# mvn clean package
 java -jar doraemon-generator/target/doraemon-generator.jar $1
 
-# mvn clean deploy 
+mvn clean deploy 
 # mvn clean install 
 
 
